2d4b14639a9ffc472607a99a9a54c96a0ed23bb6
Jack <1187625098@qq.com>
2021-09-09 23:07:58 +0800

    mybatis多数据源

---
+++ springboot-jdbc/springboot-jdbc-multiple-datasources/src/main/java/com/zjq/mybatis/MybatisMultipleApplication.java
@@ -0,0 +1,18 @@
+package com.zjq.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * mybatis多数据源
+ * @author zjq
+ */
+@SpringBootApplication
+public class MybatisMultipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisMultipleApplication.class, args);
+	}
+
+}
