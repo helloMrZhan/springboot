cb02586f14b00dce5f44e8c25d0ecb6a807354fe
zhanjq <1187625098@qq.com>
2021-09-11 20:57:31 +0800

    mybatis-plus

---
+++ springboot-jdbc/springboot-jdbc-mybatis-plus/src/main/java/com/zjq/mybatisplus/service/UserService.java
@@ -0,0 +1,14 @@
+package com.zjq.mybatisplus.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zjq.mybatisplus.entity.User;
+
+/**
+ * <p>用户</p>
+ *
+ * @Author zjq
+ * @Date 2021/9/9
+ */
+public interface UserService extends IService<User> {
+
+}
