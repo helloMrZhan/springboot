df50091483288b277aae4cdcc0ff4fff8986b6f0
Jack <1187625098@qq.com>
2021-09-24 21:07:27 +0800

    springboot actuator

---
+++ springboot-actuator/src/main/java/com/zjq/actuator/ActuatorApplication.java
@@ -0,0 +1,18 @@
+package com.zjq.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zjq
+ * @date 2021/9/18 21:16
+ * <p>title: actuator</p>
+ * <p>description: actuator</p>
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class,args);
+    }
+}
