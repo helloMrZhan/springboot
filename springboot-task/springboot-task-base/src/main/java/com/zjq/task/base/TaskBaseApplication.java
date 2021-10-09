7b581caee24aab4d78dff65328298aacfc95d19b
zhanjq <1187625098@qq.com>
2021-10-09 19:49:12 +0800

    springboot基础调度

---
+++ springboot-task/springboot-task-base/src/main/java/com/zjq/task/base/TaskBaseApplication.java
@@ -0,0 +1,18 @@
+package com.zjq.task.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>spring自带的调度</p>
+ *
+ * @Author zjq
+ * @Date 2021/10/8
+ */
+@SpringBootApplication
+public class TaskBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBaseApplication.class,args);
+    }
+}
