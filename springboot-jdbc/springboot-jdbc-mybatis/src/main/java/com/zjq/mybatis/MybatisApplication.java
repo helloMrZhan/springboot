a0e8d55d33de6b4c530195fb9aad6c80a8b4ea51
zjq <1187625098@qq.com>
2023-05-25 15:13:23 +0800

    添加注释

--- springboot-jdbc/springboot-jdbc-mybatis/src/main/java/com/zjq/mybatis/MybatisApplication.java
+++ springboot-jdbc/springboot-jdbc-mybatis/src/main/java/com/zjq/mybatis/MybatisApplication.java
@@ -3,6 +3,10 @@ package com.zjq.mybatis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot整合MyBatis
+ * @author zjq
+ */
 @SpringBootApplication
 public class MybatisApplication {
 
