d2405b152e04b33a62cef40cddd7fa4aac17affb
zjq <1187625098@qq.com>
2024-05-25 21:43:31 +0800

    基础CURD

---
+++ springboot-jdbc/springboot-jdbc-mybatis/src/main/java/com/zjq/mybatis/service/impl/UserXMLServiceImpl.java
@@ -0,0 +1,72 @@
+package com.zjq.mybatis.service.impl;
+
+import com.google.common.collect.Sets;
+import com.zjq.mybatis.entity.User;
+import com.zjq.mybatis.mapper.UserMapper;
+import com.zjq.mybatis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * <p>用户</p>
+ *
+ * @Author zjq
+ * @Date 2021/9/9
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Override
+	public int add(User user) {
+		return this.userMapper.add(user);
+	}
+
+	@Override
+	public int update(User user) {
+		return this.userMapper.update(user);
+	}
+
+	@Override
+	public List<User> list() {
+		return this.userMapper.list();
+	}
+
+	@Override
+	public int deleteById(Long userId) {
+		return this.userMapper.deleteById(userId);
+	}
+
+	@Override
+	public User getById(Long userId) {
+		return this.userMapper.getById(userId);
+	}
+
+	@Override
+	public void addAndUpdate(User user) {
+		addAndUpdateUser(user);
+	}
+
+
+	@Transactional
+	private void addAndUpdateUser(User user){
+
+	}
+
+	public static void main(String[] args) {
+		boolean contains1 = Sets.newHashSet("02", "03").contains("02");
+		System.out.println(contains1);
+		HashSet<Object> set = new HashSet<>();
+		set.add("02");
+		set.add("03");
+		boolean contains = set.contains("02");
+		System.out.println(contains);
+	}
+
+}
